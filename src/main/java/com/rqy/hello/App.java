c85eb65b7c1b6ca1c3124adc1d1fdb737c4d5d1e
rqy <rqy@itcast.cn>
2019-08-20 14:20:26 +0800

    第一次提交

---
+++ src/main/java/com/rqy/hello/App.java
@@ -0,0 +1,14 @@
+package com.rqy.hello;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		MultipartFile file = null;
+		System.out.println("Hello World!");
+	}
+}
